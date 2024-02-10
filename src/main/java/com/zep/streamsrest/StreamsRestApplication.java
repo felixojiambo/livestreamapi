dbcdfd7a93fbe9e05cc00d76f67406c3a9739c51
Felix Ojiambo <felixojiamboe@gmail.com>
2024-02-09 23:17:34 -0800

    initial setup

---
+++ src/main/java/com/zep/streamsrest/StreamsRestApplication.java
@@ -0,0 +1,13 @@
+package com.zep.streamsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zep/streamsrest/StreamsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zep.streamsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
